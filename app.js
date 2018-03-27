4f6a01ed10a52fda0bffffd689b9383f66835e28
Rocket-Hyun <rockethyun@gmail.com>
2018-03-28 00:06:57 +0900

    subway alaram added

--- app.js
+++ app.js
@@ -2,24 +2,48 @@ var express = require('express');
 var app = express();
 
 var fs = require('fs');
-var rawKey = fs.readFileSync('./credentials/bus_service_key.json');
-var serviceKey = JSON.parse(rawKey)["serviceKey"];
-var busId = JSON.parse(rawKey)["busRouteId"];
+var busRawKey = fs.readFileSync('./credentials/bus_service_key.json');
+var busServiceKey = JSON.parse(busRawKey)["serviceKey"];
+var busId = JSON.parse(busRawKey)["busRouteId"];
+
+var subwayRawKey = fs.readFileSync('./credentials/subway_service_key.json');
+// var subwayServiceKey = JSON.parse(subwayRawKey)["serviceKey"];
+var subwayServiceKey = "sample";
+var subwayId = JSON.parse(subwayRawKey)["subwayStationId"];
 
 
-const API_URL = "http://ws.bus.go.kr/api/rest/arrive/getArrInfoByRouteAll?serviceKey=" + serviceKey;
+const BUS_API_URL = "http://ws.bus.go.kr/api/rest/arrive/getArrInfoByRouteAll?serviceKey=" + busServiceKey;
+const SUBWAY_API_URL = "http://swopenapi.seoul.go.kr/api/subway/" + subwayServiceKey + "/xml/realtimeStationArrival/1/5/" + subwayId;
+
 var request = require('request');
 var parseString = require('xml2js').parseString;
 var queryParams = {busRouteId: busId};
 
 app.get('/api/bus', function(req, res){
-    request({url:API_URL, qs:queryParams}, function(err, response, body){
+    request({url:BUS_API_URL, qs:queryParams}, function(err, response, body){
         if(err) { console.log(err); return; }
 
         parseString(body, function (err, result) {
             if(err) { console.log(err); return; }
-            console.log(result.ServiceResult.msgBody[0].itemList[19]);
             res.json(result.ServiceResult.msgBody[0].itemList[19]);
+        });
+    });
+});
+
+app.get('/api/subway/:direction', function(req, res){
+    var dir = req.params.direction;
+    request(SUBWAY_API_URL, function(err, response, body){
+        if(err) { console.log(err); return; }
+
+        parseString(body, function (err, result) {
+            if(err) { console.log(err); return; }
+
+            if(dir === "down") {
+                results = [result.realtimeStationArrival.row[0].barvlDt[0], result.realtimeStationArrival.row[2].barvlDt[0]]
+            } else {
+                results = [result.realtimeStationArrival.row[1].barvlDt[0], result.realtimeStationArrival.row[3].barvlDt[0]]
+            }
+            res.json(results);
         });
     });
 });
